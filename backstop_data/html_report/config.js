ae1b4cd51e14f9f450a17f403c2e42fc4c03a2f7
Mariana Bezerra da Silva <mariana.bezerra.da.silva.01@gmail.com>
2023-05-03 00:58:24 -0300

    Update

---
+++ backstop_data/html_report/config.js
@@ -0,0 +1,193 @@
+report({
+  "testSuite": "BackstopJS",
+  "tests": [
+    {
+      "pair": {
+        "reference": "..\\bitmaps_reference\\Ticketbox_BackstopJS_Homepage_0_document_0_tablet.png",
+        "test": "..\\bitmaps_test\\20210622-172754\\Ticketbox_BackstopJS_Homepage_0_document_0_tablet.png",
+        "selector": "document",
+        "fileName": "Ticketbox_BackstopJS_Homepage_0_document_0_tablet.png",
+        "label": "BackstopJS Homepage",
+        "requireSameDimensions": true,
+        "misMatchThreshold": 0,
+        "url": "https://ticketbox-backstopjs-tat.s3.eu-central-1.amazonaws.com/index.html",
+        "expect": 0,
+        "viewportLabel": "tablet",
+        "diff": {
+          "isSameDimensions": true,
+          "dimensionDifference": {
+            "width": 0,
+            "height": 0
+          },
+          "misMatchPercentage": "0.00"
+        }
+      },
+      "status": "pass"
+    },
+    {
+      "pair": {
+        "reference": "..\\bitmaps_reference\\Ticketbox_Check_VIP_0_document_0_tablet.png",
+        "test": "..\\bitmaps_test\\20210622-172754\\Ticketbox_Check_VIP_0_document_0_tablet.png",
+        "selector": "document",
+        "fileName": "Ticketbox_Check_VIP_0_document_0_tablet.png",
+        "label": "Check VIP",
+        "requireSameDimensions": true,
+        "misMatchThreshold": 0,
+        "url": "https://ticketbox-backstopjs-tat.s3.eu-central-1.amazonaws.com/index.html",
+        "expect": 0,
+        "viewportLabel": "tablet",
+        "diff": {
+          "isSameDimensions": true,
+          "dimensionDifference": {
+            "width": 0,
+            "height": 0
+          },
+          "misMatchPercentage": "0.00"
+        }
+      },
+      "status": "pass"
+    },
+    {
+      "pair": {
+        "reference": "..\\bitmaps_reference\\Ticketbox_Checkboxes_0_document_0_tablet.png",
+        "test": "..\\bitmaps_test\\20210622-172754\\Ticketbox_Checkboxes_0_document_0_tablet.png",
+        "selector": "document",
+        "fileName": "Ticketbox_Checkboxes_0_document_0_tablet.png",
+        "label": "Checkboxes",
+        "requireSameDimensions": true,
+        "misMatchThreshold": 0,
+        "url": "https://ticketbox-backstopjs-tat.s3.eu-central-1.amazonaws.com/index.html",
+        "expect": 0,
+        "viewportLabel": "tablet",
+        "diff": {
+          "isSameDimensions": true,
+          "dimensionDifference": {
+            "width": 0,
+            "height": 0
+          },
+          "misMatchPercentage": "0.00"
+        }
+      },
+      "status": "pass"
+    },
+    {
+      "pair": {
+        "reference": "..\\bitmaps_reference\\Ticketbox_Select_Three_Tickets_0_document_0_tablet.png",
+        "test": "..\\bitmaps_test\\20210622-172754\\Ticketbox_Select_Three_Tickets_0_document_0_tablet.png",
+        "selector": "document",
+        "fileName": "Ticketbox_Select_Three_Tickets_0_document_0_tablet.png",
+        "label": "Select Three Tickets",
+        "requireSameDimensions": true,
+        "misMatchThreshold": 0,
+        "url": "https://ticketbox-backstopjs-tat.s3.eu-central-1.amazonaws.com/index.html",
+        "expect": 0,
+        "viewportLabel": "tablet",
+        "diff": {
+          "isSameDimensions": true,
+          "dimensionDifference": {
+            "width": 0,
+            "height": 0
+          },
+          "misMatchPercentage": "0.00"
+        }
+      },
+      "status": "pass"
+    },
+    {
+      "pair": {
+        "reference": "..\\bitmaps_reference\\Ticketbox_Invalid_email_0_document_0_tablet.png",
+        "test": "..\\bitmaps_test\\20210622-172754\\Ticketbox_Invalid_email_0_document_0_tablet.png",
+        "selector": "document",
+        "fileName": "Ticketbox_Invalid_email_0_document_0_tablet.png",
+        "label": "Invalid email",
+        "requireSameDimensions": true,
+        "misMatchThreshold": 0,
+        "url": "https://ticketbox-backstopjs-tat.s3.eu-central-1.amazonaws.com/index.html",
+        "expect": 0,
+        "viewportLabel": "tablet",
+        "diff": {
+          "isSameDimensions": true,
+          "dimensionDifference": {
+            "width": 0,
+            "height": 0
+          },
+          "misMatchPercentage": "0.00"
+        }
+      },
+      "status": "pass"
+    },
+    {
+      "pair": {
+        "reference": "..\\bitmaps_reference\\Ticketbox_Fill_all_mandatory_fields_0_document_0_tablet.png",
+        "test": "..\\bitmaps_test\\20210622-172754\\Ticketbox_Fill_all_mandatory_fields_0_document_0_tablet.png",
+        "selector": "document",
+        "fileName": "Ticketbox_Fill_all_mandatory_fields_0_document_0_tablet.png",
+        "label": "Fill all mandatory fields",
+        "requireSameDimensions": true,
+        "misMatchThreshold": 0,
+        "url": "https://ticketbox-backstopjs-tat.s3.eu-central-1.amazonaws.com/index.html",
+        "expect": 0,
+        "viewportLabel": "tablet",
+        "diff": {
+          "isSameDimensions": true,
+          "dimensionDifference": {
+            "width": 0,
+            "height": 0
+          },
+          "misMatchPercentage": "0.00",
+          "analysisTime": 68
+        }
+      },
+      "status": "pass"
+    },
+    {
+      "pair": {
+        "reference": "..\\bitmaps_reference\\Ticketbox_Successfully_submits_the_form_0_document_0_tablet.png",
+        "test": "..\\bitmaps_test\\20210622-172754\\Ticketbox_Successfully_submits_the_form_0_document_0_tablet.png",
+        "selector": "document",
+        "fileName": "Ticketbox_Successfully_submits_the_form_0_document_0_tablet.png",
+        "label": "Successfully submits the form",
+        "requireSameDimensions": true,
+        "misMatchThreshold": 0,
+        "url": "https://ticketbox-backstopjs-tat.s3.eu-central-1.amazonaws.com/index.html",
+        "expect": 0,
+        "viewportLabel": "tablet",
+        "diff": {
+          "isSameDimensions": true,
+          "dimensionDifference": {
+            "width": 0,
+            "height": 0
+          },
+          "misMatchPercentage": "0.00"
+        }
+      },
+      "status": "pass"
+    },
+    {
+      "pair": {
+        "reference": "..\\bitmaps_reference\\Ticketbox_SingularBaby_Homepage_0_document_0_tablet.png",
+        "test": "..\\bitmaps_test\\20210622-172754\\Ticketbox_SingularBaby_Homepage_0_document_0_tablet.png",
+        "selector": "document",
+        "fileName": "Ticketbox_SingularBaby_Homepage_0_document_0_tablet.png",
+        "label": "SingularBaby Homepage",
+        "requireSameDimensions": true,
+        "misMatchThreshold": 0,
+        "url": "http://mcstaging.singularbaby.com.br",
+        "expect": 0,
+        "viewportLabel": "tablet",
+        "diff": {
+          "isSameDimensions": false,
+          "dimensionDifference": {
+            "width": 0,
+            "height": -810
+          },
+          "misMatchPercentage": "30.15",
+          "analysisTime": 467
+        },
+        "diffImage": "..\\bitmaps_test\\20210622-172754\\failed_diff_Ticketbox_SingularBaby_Homepage_0_document_0_tablet.png"
+      },
+      "status": "fail"
+    }
+  ],
+  "id": "Ticketbox"
+});
